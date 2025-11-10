c91d623ce3842a42bcdb83cb63c0537f57a26343
kaito24mako <kaitowatanabemcc@gmail.com>
2025-11-10 18:58:12 +1100

    merge sidebar button and logo into one flex item

--- src/js/sidebar.js
+++ src/js/sidebar.js
@@ -1,22 +1,18 @@
 export function toggleSidebar() {
     const sidebar = document.querySelector('#sidebar-container');
-    const sidebarBtn = document.querySelector('#sidebarToggle');
-    const logo = document.querySelector('#logo-and-title');
+    const sidebarBtn = document.querySelector('#sidebarBtn');
+    const logo = document.querySelector('#logo');
     sidebar.classList.add('open');
 
     sidebarBtn.addEventListener('click', () => {
         if (sidebar.classList.contains('open')) {   
             /* close sidebar */   
             sidebar.classList.replace('open', 'close');
-            logo.style.position = 'relative';
-            logo.style.top = '2rem';
             logo.style.left = '4rem';
         } else {
             /* open sidebar */
             sidebar.classList.replace('close', 'open');
-            logo.style.position = 'relative';
-            logo.style.top = '2rem';
-            logo.style.left = '1rem';
+            logo.style.left = '19.5rem';
         }
     })
 }
